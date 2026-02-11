deb165752ad6ab786f1936208945960f1c24c7a4
Helene Syre <syrehelene@gmail.com>
2026-02-11 15:14:40 +0100

    Add create-post card, icons, and placeholder img
    Introduce a new createPostCard component (src/assets/js/components/createPostCard.js) that renders a post-creation UI with avatar, action buttons and a “Create a post” CTA. Add three SVG icon modules (imageIcon, videoIcon, eventIcon) and a placeholder image. Added the component to the home page (src/pages/home.js) and add a .card component style in src/style.css under @layer components to provide base styling for new cards.

---
+++ src/assets/js/components/createPostCard.js
@@ -0,0 +1,36 @@
+import gradientImg from '../../images/gradient.jpg';
+import { imageIcon } from './icons/imageIcon';
+import { videoIcon } from './icons/videoIcon';
+import { eventIcon } from './icons/eventIcon';
+
+export function createPostCard() {
+  return `
+    <section class="card mb-6">
+      <div class="flex items-center gap-4 mb-5">
+        <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-10 h-10">
+        <button onclick="" class="flex-1 text-left bg-surface-medium rounded-lg p-4 text-gray-medium">
+          What's on your mind?
+        </button>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="text-gray-light flex gap-3">
+          <button class="your-styles">
+            ${imageIcon}
+          </button>
+          <button class="your-styles">
+            ${videoIcon}
+          </button>
+          <button class="your-styles">
+            ${eventIcon}
+          </button>
+        </div>
+        <button class="rounded-lg bg-main-neon px-4 py-2 font-medium text-black transform transition-transform duration-200 hover:scale-105 hover:cursor-pointer">
+          Create a post
+        </button>
+        <!-- Other post options here -->
+      </div>
+      <!-- Other icons/buttons here -->
+    </section>
+  `;
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import { feedNavigation } from "../assets/js/components/feedNavigation";
+import { createPostCard } from "../assets/js/components/createPostCard";
 
 export function home() {
   return `
     ${feedNavigation()}
     <div class="home">
+      ${createPostCard()}
       <h1 class="text-3xl font-bold mb-4">Home Page</h1>
       <p class="text-lg">Welcome to the home page! Here you can find the latest updates and news.</p>
     </div>
