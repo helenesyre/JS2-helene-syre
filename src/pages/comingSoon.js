a091a277a0bdf27da651d2b1b2005a975e489849
Helene Syre <syrehelene@gmail.com>
2026-02-14 17:26:37 +0100

    Updated coming-soon and page/layout

--- src/pages/comingSoon.js
+++ src/pages/comingSoon.js
@@ -1,8 +1,14 @@
+import comingSoonImg from '../assets/images/coming-soon.png';
+
 export function comingSoon() {
   return `
-    <div class="coming-soon">
-      <h1 class="text-3xl font-bold mb-4">Coming Soon</h1>
-      <p class="text-lg">This feature is coming soon. Stay tuned!</p>
-    </div>
+    <section class="flex flex-col items-center justify-center gap-6 py-12 h-screen">
+      <img src="${comingSoonImg}" alt="Coming Soon" class="w-1/2 h-auto object-cover">
+      <div class="flex flex-col items-center gap-2">
+        <h1 class="text-3xl text-main-neon font-bold">Coming Soon</h1>
+        <p>This feature is coming soon. Stay tuned!</p>
+      </div>
+      <a href="#/" class="text-base rounded-lg bg-main-neon px-4 py-2 font-medium text-black transform transition-transform duration-200 hover:scale-105 hover:cursor-pointer">Go to Home</a>
+    </section>
   `;
-}
+};
