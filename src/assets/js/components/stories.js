5eae7fc33092c1fdf5dc97d670a431a0457fa3d8
Helene Syre <syrehelene@gmail.com>
2026-02-20 13:44:08 +0100

    Updated suggestions and stories
    Added hardcoded images to make the page look more finished. These are not functional at this point.

--- src/assets/js/components/stories.js
+++ src/assets/js/components/stories.js
@@ -1,13 +1,16 @@
 import gradientImg from '../../images/gradient.jpg';
 import { createIcon } from "./icons/createIcon";
 
+const storyImg1 = "https://images.unsplash.com/photo-1560942485-b2a11cc13456?q=80&w=1336&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const storyImg2 = "https://images.unsplash.com/photo-1533240332313-0db49b459ad6?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
 export function stories() {
   return `
     <section class="mb-10">
       <h2 class="text-lg mb-4 pb-2 border-b border-gray-medium">Stories</h2>
       <div class="flex justify-between">
         <div class="flex flex-col items-center gap-2">
-          <div class="rounded-full border-1 border-main-neon p-1 flex items-center justify-center">
+          <div class="rounded-full border-1 border-main-neon p-1 flex items-center justify-center hover:border-main-white hover:scale-105 cursor-pointer smooth-transition">
             <div class="rounded-full text-black bg-main-neon flex items-center justify-center w-12 h-12">
               <span class="scale-110">${createIcon}</span>
             </div>
@@ -15,16 +18,16 @@ export function stories() {
           <p class="text-sm text-gray-light"> Add Story </p>
         </div>
         <div class="flex flex-col items-center gap-2">
-          <div class="rounded-full border-1 border-main-neon p-1 flex items-center justify-center">
-            <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-12 h-12 object-cover">
+          <div class="rounded-full border-1 border-main-neon p-1 flex items-center justify-center hover:border-main-white hover:scale-105 cursor-pointer smooth-transition">
+            <img src="${storyImg1}" alt="User story" class="rounded-full w-12 h-12 object-cover">
           </div>
-          <p class="text-sm text-gray-light"> Story 2 </p>
+          <p class="text-sm text-gray-light">Isabella_B</p>
         </div>
         <div class="flex flex-col items-center gap-2">
-          <div class="rounded-full border-1 border-main-neon p-1 flex items-center justify-center">
-            <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-12 h-12 object-cover">
+          <div class="rounded-full border-1 border-main-neon p-1 flex items-center justify-center hover:border-main-white hover:scale-105 cursor-pointer smooth-transition">
+            <img src="${storyImg2}" alt="User story" class="rounded-full w-12 h-12 object-cover">
           </div>
-          <p class="text-sm text-gray-light"> Story 3 </p>
+          <p class="text-sm text-gray-light">Nova</p>
         </div>
       </div>
     </section>
--- src/assets/js/components/suggestions.js
+++ src/assets/js/components/suggestions.js
@@ -1,33 +1,39 @@
 import gradientImg from '../../images/gradient.jpg';
 
 export function suggestions() {
+  const avatarImg1 = "https://images.unsplash.com/photo-1770701195265-8af0dc148446?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const avatarImg2 = "https://images.unsplash.com/photo-1769863467291-23cf7902f686?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const avatarImg3 = "https://images.unsplash.com/photo-1685871286419-58e4fc0de8e1?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const avatarImg4 = "https://images.unsplash.com/photo-1684139517679-032b7213ad2e?q=80&w=2428&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const avatarImg5 = "https://images.unsplash.com/photo-1679193559904-aea078fa7afd?q=80&w=2396&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
   return `
     <section class="mb-6">
       <h2 class="text-lg mb-4 pb-2 border-b border-gray-medium">Suggestions</h2>
       <ul class="space-y-4">
         <li class="flex items-center gap-4">
-          <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-8 h-8">
-          <p class="text-sm font-medium">User 1</p>
+          <img src="${avatarImg1}" alt="User Avatar" class="rounded-full w-8 h-8 object-cover">
+          <p class="text-sm font-medium">MichaelAnders</p>
           <p class="ml-auto text-sm text-main-neon hover:cursor-pointer hover:underline">Follow</p>
         </li>
         <li class="flex items-center gap-4">
-          <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-8 h-8">
-          <p class="text-sm font-medium">User 2</p>
+          <img src="${avatarImg2}" alt="User Avatar" class="rounded-full w-8 h-8 object-cover">
+          <p class="text-sm font-medium">Alex_Morgan</p>
           <p class="ml-auto text-sm text-main-neon hover:cursor-pointer hover:underline">Follow</p>
         </li>
         <li class="flex items-center gap-4">
-          <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-8 h-8">
-          <p class="text-sm font-medium">User 3</p>
+          <img src="${avatarImg3}" alt="User Avatar" class="rounded-full w-8 h-8 object-cover">
+          <p class="text-sm font-medium">NightXP</p>
           <p class="ml-auto text-sm text-main-neon hover:cursor-pointer hover:underline">Follow</p>
         </li>
         <li class="flex items-center gap-4">
-          <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-8 h-8">
-          <p class="text-sm font-medium">User 4</p>
+          <img src="${avatarImg4}" alt="User Avatar" class="rounded-full w-8 h-8 object-cover">
+          <p class="text-sm font-medium">Pixel</p>
           <p class="ml-auto text-sm text-main-neon hover:cursor-pointer hover:underline">Follow</p>
         </li>
         <li class="flex items-center gap-4">
-          <img src="${gradientImg}" alt="User Avatar" class="rounded-full w-8 h-8">
-          <p class="text-sm font-medium">User 5</p>
+          <img src="${avatarImg5}" alt="User Avatar" class="rounded-full w-8 h-8 object-cover">
+          <p class="text-sm font-medium">Emma_W</p>
           <p class="ml-auto text-sm text-main-neon hover:cursor-pointer hover:underline">Follow</p>
         </li>
       </ul>
